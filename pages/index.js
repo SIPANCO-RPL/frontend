89522ab1fce7467165559a5216392e6aa554bb54
advistasyam <advis.tasyah@ui.ac.id>
2021-11-28 23:56:51 +0700

    add login page

--- pages/index.js
+++ pages/index.js
@@ -1,127 +1,350 @@
 import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
-import { Navbar } from "../components/Navbar/Navbar"
+// import Image from "next/image"
+// import "../styles/Login.css"
+// import "../public/style/login.css"
+// import { Navbar } from "../components/Navbar/Navbar"
+import Script from "next/script"
+import styled from "@emotion/styled"
+
+const LoginDiv = styled.div`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Nunito", sans-serif;
+    overflow: hidden;
+  }
+
+  .wave {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    z-index: -1;
+  }
+
+  .container {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 2rem;
+    padding: 0 2rem;
+    margin-top: -3em;
+    margin-left: 300px;
+  }
+
+  @media screen and (max-width: 1200px) {
+    .container {
+      margin-left: 150px;
+    }
+  }
+
+  @media screen and (max-width: 900px) {
+    .container {
+      margin-left: 0px;
+    }
+  }
+
+  .img {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .login-content {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    text-align: center;
+  }
+
+  .img img {
+    width: 500px;
+    display: none;
+  }
+
+  form {
+    width: 360px;
+  }
+
+  .login-content img {
+    height: 100px;
+  }
+
+  .login-content h2 {
+    margin: 15px 0;
+    color: #ed443b;
+    /* text-transform: uppercase; */
+    font-size: 2.9rem;
+    font-weight: 800;
+  }
+
+  .login-content .input-div {
+    position: relative;
+    display: grid;
+    grid-template-columns: 7% 93%;
+    margin: 25px 0;
+    padding: 5px 0;
+    border-bottom: 2px solid #d9d9d9;
+  }
+
+  .login-content .input-div.one {
+    margin-top: 0;
+  }
+
+  .i {
+    color: #d9d9d9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .i i {
+    transition: 0.3s;
+  }
+
+  .input-div > div {
+    position: relative;
+    height: 45px;
+  }
+
+  .input-div > div > h5 {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #999;
+    font-size: 18px;
+    transition: 0.3s;
+  }
+
+  .input-div:before,
+  .input-div:after {
+    content: "";
+    position: absolute;
+    bottom: -2px;
+    width: 0%;
+    height: 2px;
+    background-color: #ed443b;
+    transition: 0.4s;
+  }
+
+  .input-div:before {
+    right: 50%;
+  }
+
+  .input-div:after {
+    left: 50%;
+  }
+
+  .input-div.focus:before,
+  .input-div.focus:after {
+    width: 50%;
+  }
+
+  .input-div.focus > div > h5 {
+    top: -10px;
+    font-size: 10px;
+  }
+
+  .input-div.focus > .i > i {
+    color: #ed443b;
+  }
+
+  .input-div > div > input {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    background: none;
+    padding: 0.5rem 0.7rem;
+    font-size: 1.2rem;
+    color: #555;
+    font-family: "Nunito", sans-serif;
+  }
+
+  .input-div.pass {
+    margin-bottom: 4px;
+  }
+
+  a {
+    display: block;
+    text-align: right;
+    text-decoration: none;
+    color: #999;
+    font-size: 0.9rem;
+    transition: 0.3s;
+  }
+
+  a:hover {
+    color: #ed443b;
+  }
+
+  .btn {
+    display: block;
+    width: 100%;
+    height: 50px;
+    border-radius: 25px;
+    outline: none;
+    border: none;
+    background-image: linear-gradient(to right, #ed443b, #ff5a51, #ed443b);
+    background-size: 200%;
+    font-size: 1.2rem;
+    color: #fff;
+    font-family: "Nunito", sans-serif;
+    text-transform: uppercase;
+    margin: 2rem 0;
+    cursor: pointer;
+    transition: 0.5s;
+    font-family: "Nunito", sans-serif;
+    font-weight: 500;
+  }
+  .btn:hover {
+    background-position: right;
+  }
+
+  @media screen and (max-width: 1050px) {
+    .container {
+      grid-gap: 5rem;
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    form {
+      width: 290px;
+    }
+
+    .login-content h2 {
+      font-size: 2.4rem;
+      margin: 8px 0;
+    }
+
+    .img img {
+      width: 400px;
+    }
+  }
+
+  @media screen and (max-width: 900px) {
+    .container {
+      grid-template-columns: 1fr;
+    }
+
+    .img {
+      display: none;
+    }
+
+    .wave {
+      display: none;
+    }
+
+    .login-content {
+      justify-content: center;
+    }
+  }
+`
 
 export default function Home() {
   return (
     <>
+      <Script
+        src="https://kit.fontawesome.com/a81368914c.js"
+        strategy="beforeInteractive"
+      />
+      <Script strategy="beforeInteractive">
+        {`
+          const inputs = document.querySelectorAll(".input");
+
+          function addcl(){
+            let parent = this.parentNode.parentNode;
+            parent.classList.add("focus");
+          }
+          
+          function remcl(){
+            let parent = this.parentNode.parentNode;
+            if(this.value == ""){
+              parent.classList.remove("focus");
+            }
+          }
+          
+          
+          inputs.forEach(input => {
+            input.addEventListener("focus", addcl);
+            input.addEventListener("blur", remcl);
+          });
+          
+        `}
+      </Script>
       <Head>
         <title>Home - SIPANCO</title>
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <div className={[styles.container, "mt-3"].join(" ")}>
-
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
+      <LoginDiv>
+        <img className="wave" src="/images/Landing/sapi.svg" alt="" />
+        <div className="container">
+          <div className="img">
+            <img src="/images/Landing/virus.svg" alt="" />
           </div>
-          <button className="btn btn-primary">Bootstrap</button>
-          <button
-            type="button"
-            className="btn btn-primary"
-            data-toggle="modal"
-            data-target="#exampleModal"
-          >
-            Launch demo modal
-          </button>
 
-          <div
-            className="modal fade"
-            id="exampleModal"
-            tabIndex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">
-                    Modal title
-                  </h5>
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+          <div className="login-content">
+            <form action="" method="POST">
+              <img src="/images/Landing/avatar.svg" alt="" />
+              <h2 className="title">Login</h2>
+              <div className="input-div one">
+                <div className="i">
+                  <i className="fas fa-user"></i>
                 </div>
-                <div className="modal-body">...</div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary">
-                    Save changes
-                  </button>
+                <div className="div">
+                  {/* <h5>Username</h5> */}
+                  <input
+                    type="text"
+                    className="input"
+                    name="username"
+                    placeholder="Username"
+                  />
                 </div>
               </div>
-            </div>
+              <div className="input-div pass">
+                <div className="i">
+                  <i className="fas fa-lock"></i>
+                </div>
+                <div className="div">
+                  {/* <h5>Password</h5> */}
+                  <input
+                    type="password"
+                    className="input"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+              {/* {% if success %} */}
+              {/* <div className="alert alert-success" role="alert">
+                Login berhasil
+              </div> */}
+              {/* <!-- <p style="color: green;">Login berhasil</p> --> */}
+              {/* {% endif %} */}
+              {/* {% if failed %} */}
+              {/* <div className="alert alert-danger" role="alert">
+                Username/Password salah. Silahkan coba lagi
+              </div> */}
+              {/* <!-- <p style="color: red;">Username/Password salah. Silahkan coba lagi</p> --> */}
+              {/* {% endif %} */}
+              <a href="/login/register">
+                Dont have an account ? Register here !
+              </a>
+              <input type="submit" className="btn" value="Login" />
+            </form>
           </div>
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <span className={styles.logo}>
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={72}
-                height={16}
-              />
-            </span>
-          </a>
-        </footer>
-      </div>
+        </div>
+      </LoginDiv>
     </>
   )
 }
